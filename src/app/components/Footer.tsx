c1ef43e6f14822a8ee552767e388c2b41bf2586a
lumijoe <spacedesign63@gmail.com>
2024-01-04 15:30:14 +0900

    fix footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,8 +3,10 @@ import Link from 'next/link'
 
 export default function Footer () {
     return (
-        <footer className='py-2 px-4 border-t mt-[200px]'>
+        <footer className='py-2 px-4 border-t mt-[200px] flex justify-between'>
+            <div></div>
             <small>&copy;2024 company name</small>
+            <div></div>
         </footer>
     )
 }
